d8a379704b9300627feae63a0c120f9ba7d98eb7
Leo <lazucen8@terpmail.umd.edu>
2019-05-02 23:33:22 -0400

    Added Screens

--- App.js
+++ App.js
@@ -1,49 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground,  Alert} from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
 
-export default class App extends React.Component {
+import Login from './screens/Login';
+import Home from './screens/Home';
+
+export default class App extends Component {
+  state = {
+    isLoggedIn: false
+  }
+
   render() {
 
-    console.log('Hello Console World!');
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={require('./assets/LoginBackground.png')} style={styles.imgBackground}>
-          <View>
-            <Text style={styles.homeName}>Money Builder</Text>
-            <Button onPress={() => { Alert.alert('You tapped the button!');}}
-              title="Login" type="outline" titleStyle={{color: 'white'}} buttonStyle={{borderColor: 'white', width: 250, marginTop: -50, alignSelf: 'center'}}/>
-            <Button title="Create Account" type="outline" titleStyle={{color: 'white'}} buttonStyle={{borderColor: 'white', width: 250, marginTop: 50, alignSelf: 'center'}}/>
-          </View>
-        </ImageBackground>
-      </View>
-    );
+    if (this.state.isLoggedIn) {
+      return <Home/>;        
+    }
+    else {
+      return <Login/>;
+    }
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imgBackground: {
-    width: '100%',
-    height: '100%',
-    flex: 1,
-    opacity: 100,
-  },
-  loginText: {
-    textAlign: 'center',
-    fontSize: 14,
-    color: '#fff',
-  },
-  homeName: {
-    fontSize: 36, 
-    color: '#fff', 
-    textAlign: 'center', 
-    lineHeight: 350,
-    fontWeight: 'bold',
-  },
-
-});
+AppRegistry.registerComponent(App, () => App);
---
+++ screens/Login.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Alert, TextInput} from 'react-native';
+import { Button } from 'react-native-elements';
+
+export default class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {loginText: '', passText: ''};
+  }
+    
+  render() {
+
+    console.log('Hello Console World!');
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={require('../assets/LoginBackground.png')} style={styles.imgBackground}>
+          <View>
+            <Text style={styles.homeName}>Money Builder</Text>
+            <TextInput
+                onChangeText = { (loginText) => this.setState({loginText}) }
+                value = { this.state.loginText }
+                placeholder = {'Username'}
+            />
+            <TextInput
+                onChangeText = { (passText) => this.setState({passText}) }
+                value = { this.state.passText }
+                placeholder = {'Password'}
+            />
+            <Button onPress={() => { Alert.alert('You tapped the button!');}}
+              title="Login" type="outline" titleStyle={{color: 'white'}} buttonStyle={{borderColor: 'white', width: 250, marginTop: -50, alignSelf: 'center'}}/>
+            <Button title="Create Account" type="outline" titleStyle={{color: 'white'}} buttonStyle={{borderColor: 'white', width: 250, marginTop: 50, alignSelf: 'center'}}/>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imgBackground: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    opacity: 100,
+  },
+  loginText: {
+    textAlign: 'center',
+    fontSize: 14,
+    color: '#fff',
+  },
+  homeName: {
+    fontSize: 36, 
+    color: '#fff', 
+    textAlign: 'center', 
+    lineHeight: 350,
+    fontWeight: 'bold',
+  },
+
+});
